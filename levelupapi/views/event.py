6e897a347b9bb1afe1f7d0c3fb4ad2be2a9f6c5f
Alex Martin <alex@subtleco.co>
2021-05-06 14:39:38 -0500

    exposed Events

---
+++ levelupapi/views/event.py
@@ -0,0 +1,130 @@
+# View module for handling requests re: events
+
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from levelupapi.models import Game, Event, Gamer
+
+class Events(ViewSet):
+
+    ################################  create  ################################
+    # Response = JSON
+
+    def create(self, request):
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+
+        event = Event()
+        event.time = request.data["time"]
+        event.date = request.data["date"]
+        event.description = request.data["description"]
+        event.organizer = gamer
+
+        game = Game.objects.get(pk=request.data['gameId'])
+        event.game = game
+
+        try:
+            event.save()
+            serializer = EventSerializer(event, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    ################################  retrieve  ################################
+    # Response = JSON
+
+    def retrieve(self, request, pk=None):
+        try: 
+            event = Event.objects.get(pk=pk)
+            serializer = EventSerializer(event, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    ################################  update  ################################
+    # Response = empty 204
+
+    def update(self, request, pk=None):
+        organizer = Gamer.objects.get(user = request.auth.user)
+
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+        event.organizer = organizer
+
+        game = Game.objects.get(pk=request.data['gameId'])
+        event.game = game
+        event.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    ################################  destroy  ################################
+    # Response = 200, 404, 500
+
+    def destroy(self, request, pk=None):
+        try:
+            event = Event.objects.get(pk=pk)
+            event.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Event.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    ################################  list  ################################
+    # Response = JSON
+
+    def list(self, request):
+        events = Event.objects.all()
+
+        # Support filtering by game
+        game = self.request.query_params.get('gameId', None)
+        if game is not None:
+            events = events.filter(game__id=game)
+
+        serializer = EventSerializer(
+            events, many=True, context={'request': request})
+        return Response(serializer.data)
+
+################################  SERIALIZERS  ################################
+
+
+class EventUserSerializer(serializers.ModelSerializer):
+    """JSON serializer for event organizer's related Django user"""
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class EventGamerSerializer(serializers.ModelSerializer):
+    """JSON serializer for event organizer"""
+    user = EventUserSerializer(many=False)
+
+    class Meta:
+        model = Gamer
+        fields = ['user']
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for games"""
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'maker', 'number_of_players', 'skill_level')
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for events"""
+    organizer = EventGamerSerializer(many=False)
+    game = GameSerializer(many=False)
+
+    class Meta:
+        model = Event
+        fields = ('id', 'game', 'organizer',
+                  'description', 'date', 'time')
